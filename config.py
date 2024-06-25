53b80e9fae44265d707592b22fd1f80f28a79bcd
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-24 22:55:47 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os;
 
-# https://fgo.square.ovh/apk/com.aniplex.fategrandorder.apk
+# https://fgo.square.ovh/apk/com.aniplex.fategrandorder.en.apk
 url_apk = "https://fgo.square.ovh/apk/com.aniplex.fategrandorder.apk"
 
 # https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder
