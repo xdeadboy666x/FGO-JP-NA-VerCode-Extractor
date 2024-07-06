f435a25da254705ac9571194e01cff62589f8dfd
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-07-06 10:56:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ from libs.python.verCode import write_verCode_data
 
 import sys
 
-if check_update():
-    download_latest()
-    decompile_apk()
-    decrypt()
-    write_verCode_data();
-else:
-    print('[App] Workflow Canceled!', file=sys.stdout)
+def main():
+    """
+    Main function to run the workflow for updating, downloading, decompiling,
+    decrypting, and writing version code data for the application.
+    """
+    try:
+        if check_update():
+            download_latest()
+            decompile_apk()
+            decrypt()
+            write_verCode_data()
+        else:
+            print('[App] Workflow Canceled!', file=sys.stdout)
+    except Exception as e:
+        print(f'[App] Workflow failed due to an error: {e}', file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
