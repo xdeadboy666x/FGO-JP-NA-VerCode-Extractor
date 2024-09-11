b6adb1f3fb7ecd92a13a8a06e49e109b4b43eee9
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-10 20:06:04 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os;
 
-# https://fgo.square.ovh/apk/com.aniplex.fategrandorder.apk
-url_apk = "https://fgo.bigcereal.com/apk/com.aniplex.fategrandorder.xapk"
+url_apk = 
+"https://fgo.bigcereal.com/apk/com.aniplex.fategrandorder.en.xapk"
 
-# https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder
-url_version = "https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder"
+url_version = "https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder.en"
 
 apk_name = url_apk.split("/")[-1]
 
-# Temp folder
 temp_folder = os.path.join(os.getcwd(), "temp")
