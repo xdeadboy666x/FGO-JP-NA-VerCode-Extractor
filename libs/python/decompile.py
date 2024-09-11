82e3228774ae9100f19d08251144a33b5a4b29d7
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-10 20:11:27 -0600

    Update decompile.py

--- libs/python/decompile.py
+++ libs/python/decompile.py
@@ -1,58 +1,62 @@
+
 import os
-import subprocess
-import sys
+import subprocess;
 import config
 
+from libs.python.logger import logger
+
+# java -jar .\apktool.jar d .\fate.apk --output '.\temp\' -f
 def decompile_apk():
-    """
-    Decompile the APK file using apktool.
-
-    This function uses the apktool.jar to decompile an APK file specified in
-    the configuration. The output is directed to a temporary folder.
-    """
     apktool = os.path.join(os.getcwd(), "libs", "java", "apktool.jar")
-    apk = os.path.join(config.temp_folder, "fate.apk")
+    apkeditor = os.path.join(os.getcwd(), "libs", "java", "apkeditor.jar")
+    apk = os.path.join(config.temp_folder, config.apk_name)
 
-    print('[App] Decompiling APK...', file=sys.stdout)
-    try:
-        subprocess.run(
-            f"java -jar {apktool} d {apk} --output ./temp/files/ -f",
-            check=True,
-            shell=True
+    if config.apk_name.endswith("xapk"):
+        logger.info("XAPK detected. Merging into APK files.")
+        result = subprocess.run(["java", "-jar", apkeditor, "m", "-i", apk, "-o", apk.replace("xapk", "apk")],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
         )
-        print('[App] Decompilation completed successfully!', file=sys.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f'[Error] Decompilation failed: {e}', file=sys.stderr)
+
+        # All data is sterr maybe is a error of apkeditor is reported
+        for line in result.stderr.splitlines():
+            if line.strip():
+                logger.info(line)
+
+        apk = apk.replace("xapk", "apk")
+    
+    logger.info("Decompiling apk...")
+
+    result = subprocess.run(["java", "-jar", apktool, "d", apk, "--output", "./temp/files/", "-f"],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+    )
+
+    for line in result.stdout.splitlines():
+            if line.strip():
+                logger.info(line)
+
+    logger.info("Decompile apk succesfully!")
+
 
 def decrypt():
-    """
-    Decrypt the files using Il2CppDumper.
-
-    This function uses the Il2CppDumper executable to decrypt specific files.
-    The required input files and output directory are specified in the configuration.
-    """
-    il2cpp = os.path.join(os.getcwd(), "libs", "Il2CppDumper", "Il2CppDumper-x86.exe")
+    il2cpp = os.path.join(os.getcwd(), "libs", "Il2CppDumper", "Il2CppDumper.exe")
     global_metadata = os.path.join(config.temp_folder, "files", "assets", "bin", "Data", "Managed", "Metadata", "global-metadata.dat")
-    libil2cpp = os.path.join(config.temp_folder, "files", "lib", "armeabi-v7a", "libil2cpp.so")
-    decrypt_folder = os.path.join(config.temp_folder, "decrypt")
+    decrypt = os.path.join(config.temp_folder, "decrypt");
 
-    print('[App] Decrypting files...', file=sys.stdout)
-    try:
-        p = subprocess.Popen(
-            f"{il2cpp} {libil2cpp} {global_metadata} {decrypt_folder}",
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=True
-        )
-        stdout, stderr = p.communicate(input=b'\n')
-        if p.returncode == 0:
-            print('[App] Decryption completed successfully!', file=sys.stdout)
-        else:
-            print(f'[Error] Decryption failed: {stderr.decode()}', file=sys.stderr)
-    except Exception as e:
-        print(f'[Error] Decryption encountered an unexpected error: {e}', file=sys.stderr)
+    logger.info("Getting available libraries directories from apk")
+    lib_dir_path = os.path.join(config.temp_folder, "files", "lib")
+    lib_folders = os.listdir(lib_dir_path)
 
-if __name__ == "__main__":
-    decompile_apk()
-    decrypt()
+    logger.info(f"ArchLibs avalibles {lib_folders}")
+    logger.info(f"Getting libil2cpp from {lib_folders[0]}")
+    lib_dir_name = lib_folders[0]
+    libil2cpp = os.path.join(config.temp_folder, "files", "lib", lib_dir_name, "libil2cpp.so")
+
+    logger.info('Decrypting Files!')
+    result = subprocess.run(
+        [il2cpp, libil2cpp, global_metadata, decrypt],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+    )
+
+    for line in result.stdout.splitlines():
+        if line.strip():
+            logger.info(line)
