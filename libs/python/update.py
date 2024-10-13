d362a633a8bb09454980990b1c900d1db034d3cc
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-10-12 23:23:23 -0600

    Update update.py

--- libs/python/update.py
+++ libs/python/update.py
@@ -8,20 +8,25 @@ from libs.python.logger import logger
 pathRoot = os.getcwd()
 pathVerCode = os.path.join(pathRoot, "VerCode.json")
 
+
 def get_version():
-    return httpx.get(config.url_version).text        
+    return httpx.get(config.url_version).text
+
 
 def is_necesary_update(current, latest):
     return current != latest
+
 
 def check_update():
     if os.path.exists(pathVerCode):
         with open(pathVerCode) as fVerCode:
             dataVerCode = json.load(fVerCode)
             versionVerCode = dataVerCode["appVer"]
-            versionAtlas = get_version() 
-            logger.info(f"Current installed version: {versionVerCode}, up-to-date with the latest available version: {versionAtlas}.")
+            versionAtlas = get_version()
+            logger.info(
+                f"Current installed version: {versionVerCode}, up-to-date with the latest available version: {versionAtlas}."
+            )
             return is_necesary_update(versionVerCode, versionAtlas)
     else:
-        
+
         return True
