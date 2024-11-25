01f5aa65e1473ba4f0ae051afab632da91cb4839
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-11-25 08:03:03 -0600

    Update download.py

--- libs/python/download.py
+++ libs/python/download.py
@@ -2,21 +2,21 @@ import os
 import httpx
 import config
 from libs.python.logger import logger
- 
- 
+
+
 def download_latest():
     logger.info("Creating folder...")
- 
+
     os.mkdir(config.temp_folder)
     os.mkdir(os.path.join(config.temp_folder, "decrypt"))
- 
+
     logger.info("Downloading latest apk...!")
- 
+
     with httpx.stream("GET", config.url_apk, follow_redirects=True) as response:
         response.raise_for_status()
- 
+
         with open(os.path.join(config.temp_folder, config.apk_name), "wb") as file:
             for chunk in response.iter_bytes():
                 file.write(chunk)
- 
+
     logger.info("Apk downloaded!")
