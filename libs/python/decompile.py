fa83188aec08cc8eb6a96d22d97005aa1fe0a636
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-11-25 08:11:13 -0600

    Update decompile.py

--- libs/python/decompile.py
+++ libs/python/decompile.py
@@ -1,16 +1,16 @@
 import os
 import subprocess
 import config
- 
+
 from libs.python.logger import logger
- 
- 
+
+
 # java -jar .\apktool.jar d .\fate.apk --output '.\temp\' -f
 def decompile_apk():
     apktool = os.path.join(os.getcwd(), "libs", "java", "apktool.jar")
     apkeditor = os.path.join(os.getcwd(), "libs", "java", "apkeditor.jar")
     apk = os.path.join(config.temp_folder, config.apk_name)
- 
+
     if config.apk_name.endswith("xapk"):
         logger.info("XAPK detected. Merging into APK files.")
         result = subprocess.run(
@@ -28,30 +28,30 @@ def decompile_apk():
             stderr=subprocess.PIPE,
             text=True,
         )
- 
+
         # All data is sterr maybe is a error of apkeditor is reported
         for line in result.stderr.splitlines():
             if line.strip():
                 logger.info(line)
- 
+
         apk = apk.replace("xapk", "apk")
- 
+
     logger.info("Decompiling apk...")
- 
+
     result = subprocess.run(
         ["java", "-jar", apktool, "d", apk, "--output", "./temp/files/", "-f"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
     )
- 
+
     for line in result.stdout.splitlines():
         if line.strip():
             logger.info(line)
- 
+
     logger.info("Decompile apk succesfully!")
- 
- 
+
+
 def decrypt():
     il2cpp = os.path.join(os.getcwd(), "libs", "Il2CppDumper", "Il2CppDumper.exe")
     global_metadata = os.path.join(
@@ -65,18 +65,18 @@ def decrypt():
         "global-metadata.dat",
     )
     decrypt = os.path.join(config.temp_folder, "decrypt")
- 
+
     logger.info("Getting available libraries directories from apk")
     lib_dir_path = os.path.join(config.temp_folder, "files", "lib")
     lib_folders = os.listdir(lib_dir_path)
- 
+
     logger.info(f"ArchLibs avalibles {lib_folders}")
     logger.info(f"Getting libil2cpp from {lib_folders[0]}")
     lib_dir_name = lib_folders[0]
     libil2cpp = os.path.join(
         config.temp_folder, "files", "lib", lib_dir_name, "libil2cpp.so"
     )
- 
+
     logger.info("Decrypting Files!")
     result = subprocess.run(
         [il2cpp, libil2cpp, global_metadata, decrypt],
@@ -84,7 +84,7 @@ def decrypt():
         stderr=subprocess.PIPE,
         text=True,
     )
- 
+
     for line in result.stdout.splitlines():
         if line.strip():
             logger.info(line)
