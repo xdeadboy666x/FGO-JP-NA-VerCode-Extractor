518ae15b0e3aa9d973813d3695acdfa9d1f976e6
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-12-20 01:42:59 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 # https://fgo.square.ovh/apk/com.aniplex.fategrandorder.en.apk
-url_apk = "https://fgo.bigcereal.com/apk/com.aniplex.fategrandorder.en.xapk"
+url_apk = "https://storage.evozi.com/apk/dl/17/06/25/com.aniplex.fategrandorder.en_131.apk"
 
 # https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder
 url_version = (
-    "https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder.en"
+    "https://play.google.com/store/apps/details?id=com.aniplex.fategrandorder.en&device=phone"
 )
 
 apk_name = url_apk.split("/")[-1]
